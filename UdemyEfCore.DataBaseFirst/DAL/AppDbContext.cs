b26ee216336405681eee22783a2decd8a065f67f
hsnbsrtrhn <hasant_basri@outlook.com>
2024-11-12 21:39:47 +0300

    First commit for this project

---
+++ UdemyEfCore.DataBaseFirst/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace UdemyEfCore.Data.DAL;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    public AppDbContext()
+    {
+    }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(DbContextInitializer.Configuration.GetConnectionString("SqlCon"));
+    }
+}
---
+++ UdemyEfCore.DataBaseFirst/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyEfCore.Data.DAL;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Build the configuration
+        DbContextInitializer.Build();
+
+        using (var context = new AppDbContext())
+        {
+            var products = context.Products.ToList();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product Name: {product.Name}, Price: {product.Price}");
+            }
+        }
+    }
+}
+
